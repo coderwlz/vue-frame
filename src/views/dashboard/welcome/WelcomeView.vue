28d4bed2f4e8bd3ad6173f4b2d07b6afa291a429
wanglongzhen <1962955586@qq.com>
2023-05-12 09:17:39 +0800

    fix: logo

--- src/views/dashboard/welcome/WelcomeView.vue
+++ src/views/dashboard/welcome/WelcomeView.vue
@@ -1,3 +1,38 @@
 <template>
   <div><a-button class="mt-2" type="primary">Primary Button</a-button></div>
+  <a-table :dataSource="dataSource" :columns="columns" />
 </template>
+<script setup>
+import { ref } from 'vue'
+const dataSource = ref([
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号'
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号'
+  }
+])
+const columns = ref([
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age'
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address'
+  }
+])
+</script>
